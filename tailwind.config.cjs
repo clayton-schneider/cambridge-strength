2d7c674ecd41981cc0389e39cf003ef5760d76ba
clayton-schneider <schneiderc2016@gmail.com>
2023-01-10 10:45:42 -0500

    google font setups

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+const FONT_FAMILY_BASE = [
+  "system-ui",
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "Segoe UI",
+  "Roboto",
+  "Oxygen",
+  "Ubuntu",
+  "Cantarell",
+  "Open Sans",
+  "Helvetica Neue",
+  "sans-serif",
+];
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6BFA0C",
+      },
+      padding: { edge: "2.5%" },
+      fontFamily: {
+        sans: ["Inter", ...FONT_FAMILY_BASE],
+        anton: ["Anton", ...FONT_FAMILY_BASE],
+      },
+    },
+  },
+  plugins: [],
+};
